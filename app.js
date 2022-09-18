541f6911880b7f9155616d8370b47d348a24750e
Ananias Barbosa <ananiasgomes27@gmail.com>
2022-09-18 18:09:13 -0300

    chore: testes finalizados

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
-  const moeda = await cotacao.extractValor();
+  const moeda = await cotacao.getCotacao();
 
   res.render("pages/index", {
     moeda,
